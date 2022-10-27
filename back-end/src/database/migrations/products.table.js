4d4637ed133f0b701f6a0308f2671941a6615c5c
klauslube <klauslube93@gmail.com>
2022-10-27 19:11:17 -0300

    feat: criaçao migration de products

---
+++ back-end/src/database/migrations/products.table.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: async (QueryInterface, Sequelize) => {
+    await QueryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      url_image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (QueryInterface, _Sequelize) => {
+    await QueryInterface.dropTable('products');
+  },
+}
