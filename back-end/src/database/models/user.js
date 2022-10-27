9cda2921599e8656c0923987a5d1e90e47971368
klauslube <klauslube93@gmail.com>
2022-10-27 17:06:28 -0300

    feat: criaçao do model User

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type:DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false,
+    tableName: 'users'
+  });
+
+  return User;
+};
