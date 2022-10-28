1dcb0afdf975f30378abb9eee9bf0144c455cb86
Rafael Moraes <rafaelsm003@gmail.com>
2022-10-27 23:55:36 -0300

    Revert "Main group 03 inputs login frontend"

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -1,50 +1,9 @@
-import { useState } from 'react';
-import Input from '../../components/Input';
+import React from 'react';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const validateFields = () => {
-    const MIN_PASSWORD_LENGTH = 6;
-    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    return regex.test(email) && password.length >= MIN_PASSWORD_LENGTH;
-  };
-
-  console.log(email, password);
-
   return (
     <div>
-
-      <Input
-        testId="common_login__input-email"
-        type="email"
-        labelText="Login"
-        onChange={ setEmail }
-      />
-
-      <Input
-        testId="common_login__input-password"
-        type="text"
-        labelText="Senha"
-        onChange={ setPassword }
-      />
-
-      <button
-        data-testid="common_login__button-login"
-        type="button"
-        disabled={ !validateFields() }
-      >
-        Login
-      </button>
-
-      <button
-        data-testid="common_login__button-register"
-        type="button"
-      >
-        Ainda não tenho conta
-      </button>
-
+      <h1>Login</h1>
     </div>
   );
 }
