6b06f584b792abe6f696b0589127a80d6a12d17d
klauslube <klauslube93@gmail.com>
2022-10-28 14:54:33 -0300

    feat: criaçao model products

---
+++ back-end/src/database/models/products.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    id: { 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(4,2),
+      allowNull: false,
+    },
+    password: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    urlImage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    timestamps: false,
+    tableName: 'products'
+  });
+
+  return Product;
+};
