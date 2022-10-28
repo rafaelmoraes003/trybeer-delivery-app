5ce0095da8c511819feb86f9fac69f781b7c3436
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-10-28 17:00:43 -0300

    feat: Requisito 7 elementos da tela de registro com os data-testids disponíveis no protótipo

--- front-end/src/pages/Register/index.jsx
+++ front-end/src/pages/Register/index.jsx
@@ -2,9 +2,47 @@ import React from 'react';
 
 function Register() {
   return (
-    <div>
-      <h1>Cadastro teste</h1>
-    </div>
+    <main>
+      <h1>Cadastro</h1>
+      <form>
+        <label htmlFor="input-name">
+          Nome
+          <input
+            type="text"
+            id="input-name"
+            name="name"
+            placeholder="Seu nome"
+            data-testid="common_register__input-name"
+          />
+        </label>
+        <label htmlFor="input-email">
+          Email
+          <input
+            type="email"
+            id="input-email"
+            name="email"
+            placeholder="seu-email@site.com.br"
+            data-testid="common_register__input-email"
+          />
+        </label>
+        <label htmlFor="input-password">
+          Senha
+          <input
+            type="password"
+            id="input-password"
+            name="password"
+            placeholder="Sua-senha"
+            data-testid="common_register__input-password"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="common_register__button-register"
+        >
+          CADASTRAR
+        </button>
+      </form>
+    </main>
   );
 }
 
