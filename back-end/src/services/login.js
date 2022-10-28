7ec37153ace765bb48b0d2ca4f8f7f4915a2c3d0
rafael moraes <rafaelsm003@gmail.com>
2022-10-27 23:53:44 -0300

    feat: adiciona funcionalidade de validar se usuário existe e se sua senha descriptografada está correta

--- back-end/src/services/login.js
+++ back-end/src/services/login.js
@@ -1,9 +1,15 @@
 const { User } = require('../database/models');
+const { decryptPassword } = require('../utils/descryptPassword');
 
 const login = async (userData) => {
   const { email, password } = userData;
-  const user = await User.findOne({ where: { email, password } });
+  const user = await User.findOne({ where: { email } });
+
   if (!user) return { code: 404 };
+
+  const decryptedPassword = decryptPassword(password);
+  if (user.password !== decryptedPassword) return { code: 404 };
+
   return { code: 200 };
 }; 
 
