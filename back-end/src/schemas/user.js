a97f260a75ef95c1ccd5b871db710c1a775c39ec
rafael moraes <rafaelsm003@gmail.com>
2022-10-31 18:46:30 -0300

    fix: user joi schema

--- back-end/src/schemas/user.js
+++ back-end/src/schemas/user.js
@@ -2,7 +2,7 @@ const joi = require('joi');
 
 const userSchema = joi.object({
   name: joi.string().min(12).required(),
-  email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+  email: joi.string().email().required(),
   password: joi.string().min(6).required(),
   role: joi.string(),
 });
