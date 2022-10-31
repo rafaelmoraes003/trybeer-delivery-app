52e8637d9826bebbdc30363f4aae0d7079553c32
rafael moraes <rafaelsm003@gmail.com>
2022-10-31 18:09:42 -0300

    fix: troca middleware de autenticação de usuário por middleware de erro

--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const userController = require('../controllers/user');
-const { userMiddleware } = require('../middlewares/validation');
 
 const router = Router();
 
 router.get('/', userController.getAll);
-router.post('/', userMiddleware, userController.create);
+router.post('/', userController.create);
 
 module.exports = router;
