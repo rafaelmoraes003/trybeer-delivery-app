eec04d9925b7b0e03f72d7f20a439f0482a41846
rafael moraes <rafaelsm003@gmail.com>
2022-10-31 18:46:47 -0300

    fix: importação

--- back-end/src/services/login.js
+++ back-end/src/services/login.js
@@ -1,5 +1,5 @@
 const { User } = require('../database/models');
-const CustomError = require('../utils/CustomError');
+const { CustomError } = require('../utils/CustomError');
 const { validateBody } = require('../utils/validateBody');
 const { decryptPassword } = require('../utils/descryptPassword');
 const { loginSchema } = require('../schemas/login');
