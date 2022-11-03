d15a6777cc366de04ee0a78df4ca45c0132a06b6
klauslube <klauslube93@gmail.com>
2022-11-03 18:05:29 -0300

    feat: criado rotas de sales

--- back-end/src/routes/sale.js
+++ back-end/src/routes/sale.js
@@ -4,5 +4,10 @@ const saleController = require('../controllers/sale');
 const router = Router();
 
 router.post('/', saleController.create);
-
+router.get('/', saleController.getAll);
+router.get('/seller', saleController.getAllBySellers);
+router.get('/customer', saleController.getAllByUsers);
+router.get('/:id', saleController.getById);
+router.put('/:id', saleController.update);
+router.delete('/:id', saleController.destroy);
 module.exports = router;
