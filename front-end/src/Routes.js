f4692489806c7adffaf02e67943d0f0de8e26ea9
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-11-03 17:31:22 -0300

    feat: Criação do componente tableProducts e page checkout e a rota customer/checkout

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Routes as Switch, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Register from './pages/Register';
+import Checkout from './pages/Checkout';
 
 function Routes() {
   return (
@@ -13,6 +14,7 @@ function Routes() {
           <Route exact path="/login" element={ <Login /> } />
           <Route exact path="/register" element={ <Register /> } />
           <Route exact path="/customer/products" element={ <Products /> } />
+          <Route exact path="/customer/checkout" element={ <Checkout /> } />
         </Switch>
       </main>
     </div>
---
+++ front-end/src/pages/Checkout/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../../components/NavBar';
+import TableProducts from '../../components/TableProducts';
+
+function Checkout() {
+  return (
+    <main>
+      <NavBar />
+      <h1>Finalizar Pedido</h1>
+      <TableProducts />
+
+    </main>
+  );
+}
+
+export default Checkout;
