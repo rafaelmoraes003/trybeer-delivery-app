7fb6e4d49a0a6e885ff8dedad8ae6f3afacb49d3
klauslube <klauslube93@gmail.com>
2022-11-03 17:30:18 -0300

    feat: criado resto das funçoes CRUD no controller

--- back-end/src/controllers/sale.js
+++ back-end/src/controllers/sale.js
@@ -5,4 +5,50 @@ const create = async (req, res) => {
   return res.status(201).json(newSale);
 };
 
-module.exports = { create };
+const getAll = async (req, res) => {
+  const allsales = await saleService.getAll();
+  return res.status(200).json(allsales);
+};
+
+const getAllBySellers = async (req, res) => {
+  const { sellerId } = req.params;
+  const allsales = await saleService.getAllBySellers(sellerId);
+  return res.status(200).json(allsales);
+};
+
+const getAllByUsers = async (req, res) => {
+  const { userId } = req.params;
+  const allsales = await saleService.getAllByUsers(userId);
+  return res.status(200).json(allsales);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const sale = await saleService.getById(id);
+  return res.status(200).json(sale);
+};
+
+const update = async (req, res) => {
+  const { userId, sellerId, totalPrice,
+    deliveryAddress, deliveryNumber, saleDate, status } = req.body;
+  const { id } = req.params;
+  await saleService.update(id, 
+  { 
+    userId,
+    sellerId,
+    totalPrice,
+    deliveryAddress,
+    deliveryNumber,
+    saleDate,
+    status, 
+  });
+  return res.status(200).json({ message: 'sale updated' });
+};
+
+const destroy = async (req, res) => {
+  const { id } = req.params;
+  await saleService.destroy(id);
+  return res.status(200).json({ message: 'sale deleted' });
+};
+
+module.exports = { create, getAll, getAllBySellers, getAllByUsers, getById, update, destroy };
