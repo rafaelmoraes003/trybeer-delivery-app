fd4e702b00ab3ca8bac3ba01caac73cce3e57512
klauslube <klauslube93@gmail.com>
2022-11-03 17:22:07 -0300

    feat: criado resto das funçoes CRUD do service

--- back-end/src/services/sale.js
+++ back-end/src/services/sale.js
@@ -5,8 +5,54 @@ const { saleSchema } = require('../schemas/sale');
 const create = async (saleData) => {
   validateBody(saleData, saleSchema);
   const newSale = await Sale.create(saleData);
-  console.log('oi');
   return newSale;
 };
 
-module.exports = { create };
+const getAll = async () => {
+  const allSales = await Sale.findAll();
+  return allSales;
+};
+
+const getAllBySellers = async (sellerId) => {
+  const sales = await Sale.findAll({ where: { sellerId } });
+  return sales;
+}; 
+
+const getAllByUsers = async (userId) => {
+  const sales = await Sale.findAll({ where: { userId } });
+  return sales;
+}; 
+
+const getById = async (id) => {
+  const product = await Sale.findOne({ where: { id } });
+  // if (!product) throw new CustomError(notFound, 400);
+  return product;
+};
+
+const update = async (id, saleData) => {
+  const { userId, sellerId, totalPrice,
+    deliveryAddress, deliveryNumber, saleDate, status } = saleData;
+  validateBody(saleData, saleSchema);
+  const updatedProduct = await Sale.update(
+  { 
+    userId,
+    sellerId,
+    totalPrice,
+    deliveryAddress,
+    deliveryNumber,
+    saleDate, 
+    status, 
+  },
+  { where: { id } },
+);
+  // if (!updatedProduct) throw new CustomError(notFound, 400);
+  return updatedProduct;
+};
+
+const destroy = async (id) => {
+  const deletedProduct = await Sale.destroy({ where: { id } });
+  // if (!deletedProduct) throw new CustomError(notFound, 400);
+  return deletedProduct;
+};
+
+module.exports = { create, getAll, getAllBySellers, getAllByUsers, getById, update, destroy };
