cef339e2dfe482538affc3d750ff8c7e7b7a9153
rafael moraes <rafaelsm003@gmail.com>
2022-11-03 23:12:01 -0300

    feat: manipula dados do componete para mandá-los para a api e criar venda

--- front-end/src/components/CheckoutDetails/index.jsx
+++ front-end/src/components/CheckoutDetails/index.jsx
@@ -1,21 +1,41 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import createSaleRequest from '../../utils/createSaleRequest';
+import getTotalPrice from '../../utils/getTotalPrice';
 import InputWithLabel from '../InputWithLabel/index';
 
 function CheckoutDetails() {
+  const navigateTo = useNavigate();
   const [sellers, setSellers] = useState([]);
+  const [sellerId, setSellerId] = useState(null);
   const [address, setAddress] = useState('');
   const [number, setNumber] = useState(0);
+  const [errorMessage, setErrorMessage] = useState(false);
 
   useEffect(() => {
     const getSellers = async () => {
       const response = await fetch('http://localhost:3001/users/sellers');
       const data = await response.json();
       setSellers(data);
+      setSellerId(data[0].id);
     };
     getSellers();
   }, []);
 
-  console.log(address, number);
+  const createSale = async () => {
+    const userData = JSON.parse(localStorage.getItem('user'));
+    const totalPrice = getTotalPrice();
+
+    const body = await createSaleRequest({
+      userData, totalPrice, address, number, sellerId,
+    });
+    console.log(body);
+    if (body.error) {
+      setErrorMessage(true);
+    } else {
+      navigateTo(`/customer/orders/${body.id}`);
+    }
+  };
 
   return (
     <div>
@@ -26,6 +46,7 @@ function CheckoutDetails() {
           <select
             id="sellers"
             data-testid="customer_checkout__select-seller"
+            onChange={ (e) => console.log(e.target.value) }
           >
             {sellers.map((seller) => (
               <option
@@ -42,22 +63,28 @@ function CheckoutDetails() {
           labelText="Endereço"
           type="text"
           testId="customer_checkout__input-address"
-          onChange={ (e) => setAddress(e.target.value) }
+          onChange={ setAddress }
         />
 
         <InputWithLabel
           labelText="Número"
           type="number"
           testId="customer_checkout__input-address-number"
-          onChange={ (e) => setNumber(Number(e.target.value)) }
+          onChange={ setNumber }
         />
 
         <button
           type="button"
           data-testid="customer_checkout__button-submit-order"
+          onClick={ createSale }
         >
           Finalizar Pedido
         </button>
+        {errorMessage && (
+          <p>
+            Dados Inválidos
+          </p>
+        )}
       </div>
     </div>
   );
