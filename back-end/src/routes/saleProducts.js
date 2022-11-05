bdd88406a8724254947c5f7cf7bf5f6dd37db201
rafael moraes <rafaelsm003@gmail.com>
2022-11-05 01:25:25 -0300

    feat: cria o endpoint GET /sales-products

--- back-end/src/routes/saleProducts.js
+++ back-end/src/routes/saleProducts.js
@@ -4,5 +4,6 @@ const saleProductsController = require('../controllers/salesProducts');
 const router = Router();
 
 router.post('/', saleProductsController.create);
+router.get('/', saleProductsController.getAll);
 
 module.exports = router;
