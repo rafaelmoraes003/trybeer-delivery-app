025eb803e84f09d85b1dc9006c5dcf3ea033cf9f
rafael moraes <rafaelsm003@gmail.com>
2022-11-05 01:21:42 -0300

    feat: cria a função create no controller de salesProducts para criação de uma venda de produto

--- back-end/src/controllers/salesProducts.js
+++ back-end/src/controllers/salesProducts.js
@@ -10,4 +10,13 @@ const getAll = async (req, res, next) => {
   }
 };
 
+const create = async (req, res, next) => {
+  try {
+    const { code, data } = await saleProductsService.create(req.body);
+    return res.status(code).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = { create, getAll };
