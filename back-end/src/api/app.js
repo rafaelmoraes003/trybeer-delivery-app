83311d51873f9a177c7e5260b890e6f28c4c6922
rafael moraes <rafaelsm003@gmail.com>
2022-11-05 01:19:46 -0300

    feat: cria a rota /sales-products

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,6 +5,7 @@ const loginRoute = require('../routes/login');
 const userRoute = require('../routes/user');
 const productRoute = require('../routes/product');
 const saleRoute = require('../routes/sale');
+const saleProductsRoute = require('../routes/saleProducts');
 
 const { errorMiddleware } = require('../middlewares/errorMiddleware');
 
@@ -20,6 +21,7 @@ app.use('/login', loginRoute);
 app.use('/products', productRoute);
 app.use('/users', userRoute);
 app.use('/sales', saleRoute);
+app.use('/sales-products', saleProductsRoute);
 
 app.use(errorMiddleware);
 
