7dce43f5b2ff2ad1423189bca7c0fbf7fb66522e
rafael moraes <rafaelsm003@gmail.com>
2022-11-05 01:20:39 -0300

    feat: cria a função getAll no controller de salesProducts para listas todas as vendas de produtos

---
+++ back-end/src/controllers/salesProducts.js
@@ -0,0 +1,13 @@
+const saleProductsService = require('../services/salesProducts');
+
+
+const getAll = async (req, res, next) => {
+  try {
+    const { code, data } = await saleProductsService.getAll();
+    return res.status(code).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { create, getAll };
