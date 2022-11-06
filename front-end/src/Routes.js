f409d7da7c87a9b3448b6087e15284ad06c2bf0b
rafael moraes <rafaelsm003@gmail.com>
2022-11-06 00:54:22 -0300

    fix: remove elementos html extras nas rotas do frontend

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -4,20 +4,18 @@ import Login from './pages/Login';
 import Products from './pages/Products';
 import Register from './pages/Register';
 import Checkout from './pages/Checkout';
+import Orders from './pages/Orders';
 
 function Routes() {
   return (
-    <div>
-      <main>
-        <Switch>
-          <Route exact path="/" element={ <Navigate to="/login" /> } />
-          <Route exact path="/login" element={ <Login /> } />
-          <Route exact path="/register" element={ <Register /> } />
-          <Route exact path="/customer/products" element={ <Products /> } />
-          <Route exact path="/customer/checkout" element={ <Checkout /> } />
-        </Switch>
-      </main>
-    </div>
+    <Switch>
+      <Route exact path="/" element={ <Navigate to="/login" /> } />
+      <Route exact path="/login" element={ <Login /> } />
+      <Route exact path="/register" element={ <Register /> } />
+      <Route exact path="/customer/products" element={ <Products /> } />
+      <Route exact path="/customer/checkout" element={ <Checkout /> } />
+      <Route exact path="/customer/orders" element={ <Orders /> } />
+    </Switch>
   );
 }
 
