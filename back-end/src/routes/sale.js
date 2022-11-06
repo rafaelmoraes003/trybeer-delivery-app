31a3c457baf966bf856e8b40666e5e5a0c0c83c9
rafael moraes <rafaelsm003@gmail.com>
2022-11-06 00:52:45 -0300

    feat: cria os endpoints GET /sales/seller/:id e /sales/customer/:id

--- back-end/src/routes/sale.js
+++ back-end/src/routes/sale.js
@@ -6,8 +6,8 @@ const router = Router();
 
 router.post('/', auth, saleController.create);
 router.get('/', saleController.getAll);
-router.get('/seller', saleController.getAllSellers);
-router.get('/customer', saleController.getAllUsers);
+router.get('/seller/:id', saleController.getAllBySellerId);
+router.get('/customer/:id', saleController.getAllByUserId);
 router.get('/:id', saleController.getById);
 router.put('/:id', saleController.update);
 router.delete('/:id', saleController.destroy);
