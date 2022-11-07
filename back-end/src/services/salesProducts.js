3fd56c0c3d84711520851a8e70a77746bb9d6733
klauslube <klauslube93@gmail.com>
2022-11-07 16:07:38 -0300

    fix: mudança de status

--- back-end/src/services/salesProducts.js
+++ back-end/src/services/salesProducts.js
@@ -5,12 +5,11 @@ const { validateBody } = require('../utils/validateBody');
 const getAll = async (saleProductList) => {
   validateBody(saleProductList, saleProductSChema);
   const saleProducts = await SaleProduct.findAll();
-  return { code: 201, data: saleProducts };
+  return { code: 200, data: saleProducts };
 };
 
 const create = async (saleProductList) => {
   validateBody(saleProductList, saleProductSChema);
-  console.log(saleProductList);
   const newSaleProducts = await SaleProduct.bulkCreate(saleProductList);
   return { code: 201, data: newSaleProducts };
 };
