7310689dfebcbe1a8ea2cc04872e6691a4788708
rafael moraes <rafaelsm003@gmail.com>
2022-11-08 15:54:17 -0300

    deleta arquivos não utilizados

--- front-end/src/utils/getOrderDetails.js
+++
@@ -1,8 +0,0 @@
-const getOrderDetails = async (endpoint, id, setState) => {
-  const response = await
-  fetch(`http://localhost:3001/${endpoint}/${id}?showProducts=true`);
-  const data = await response.json();
-  setState(data);
-};
-
-export default getOrderDetails;
