d12520ed352a92235aa1056ec21deca4fbb4c957
rafael moraes <rafaelsm003@gmail.com>
2022-11-08 02:15:21 -0300

    refactor: refatora joins de função em variáveis separadas

--- back-end/src/utils/getSales.js
+++ back-end/src/utils/getSales.js
@@ -1,31 +1,37 @@
-const { Sale, User, Product, SaleProduct } = require('../database/models');
+const { Sale, User, Product } = require('../database/models');
+
+const userJOIN = {
+  model: User,
+  as: 'user',
+  attributes: { exclude: ['password', 'id', 'email', 'role'] },
+};
+
+const sellerJOIN = {
+  model: User,
+  as: 'seller',
+  attributes: { exclude: ['password', 'id', 'email', 'role'] },
+};
+
+const productsJOIN = {
+  model: Product,
+  as: 'products',
+  through: { attributes: ['quantity'] },
+  attributes: { exclude: ['urlImage'] },
+};
 
 const getSales = async (method, condition, showProducts) => {
   const sales = await Sale[method]({
     where: condition,
     ...(showProducts === 'true' && {
       include: [
-        {
-          model: User,
-          as: 'user',
-          attributes: { exclude: ['password', 'id', 'email', 'role'] },
-        },
-        {
-          model: User,
-          as: 'seller',
-          attributes: { exclude: ['password', 'id', 'email', 'role'] },
-        },
-        {
-          model: Product,
-          as: 'products',
-          through: { attributes: ['quantity'] },
-          attributes: { exclude: ['urlImage'] }
-        }
+        userJOIN,
+        sellerJOIN,
+        productsJOIN,
       ],
-    } )
+    }),
   });
 
   return sales;
-}
+};
 
 module.exports = { getSales };
