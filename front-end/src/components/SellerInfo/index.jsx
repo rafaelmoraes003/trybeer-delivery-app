d2e9e6254674c778fb28f0fc14357d6d005e138c
klauslube <klauslube93@gmail.com>
2022-11-08 18:03:20 -0300

    feat: criado logica para mudança de status do pedido pelo onClick

--- front-end/src/components/SellerInfo/index.jsx
+++ front-end/src/components/SellerInfo/index.jsx
@@ -1,6 +1,32 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function SellerInfo({ id, saleDate, status }) {
+  const [orderStatus, setOrderStatus] = useState(status);
+
+  const ChangePrepare = () => {
+    if (orderStatus === 'Pendente') return false;
+    if (orderStatus !== 'Pendente') return true;
+  };
+
+  const ChangeStatusClick = async (newStatus) => {
+    setOrderStatus(newStatus);
+    await fetch(`http://localhost:3001/sales/${id}`, {
+      method: 'PATCH',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify({
+        status: newStatus,
+      }),
+    });
+  };
+
+  const ChangeDelivery = () => {
+    if (orderStatus === 'Preparando') return false;
+    if (orderStatus !== 'Preparando') return true;
+  };
+
   return (
     <div>
       <p data-testid="seller_order_details__element-order-details-label-order-id">
@@ -12,11 +38,13 @@ function SellerInfo({ id, saleDate, status }) {
       <p
         data-testid="seller_order_details__element-order-details-label-delivery-status"
       >
-        { status }
+        { orderStatus }
       </p>
       <button
         type="button"
         data-testid="seller_order_details__button-preparing-check"
+        onClick={ () => ChangeStatusClick('Preparando') }
+        disabled={ ChangePrepare() }
       >
         Preparar Pedido
       </button>
@@ -24,7 +52,8 @@ function SellerInfo({ id, saleDate, status }) {
       <button
         type="button"
         data-testid="seller_order_details__button-dispatch-check"
-        disabled
+        onClick={ () => ChangeStatusClick('Em Trânsito') }
+        disabled={ ChangeDelivery() }
       >
         Saiu para Entrega
       </button>
