7c8ddc66dea835e87f27085e757fdd51c945dbe9
klauslube <klauslube93@gmail.com>
2022-11-10 18:30:24 -0300

    chore: comentado arquivos e funçoes que não foram utilizadas para a aplicação do frontend

--- back-end/src/controllers/product.js
+++ back-end/src/controllers/product.js
@@ -1,13 +1,13 @@
 const productService = require('../services/product');
 
-const create = async (req, res, next) => {
-  try {
-    const newProduct = await productService.create(req.body);
-    return res.status(201).json(newProduct);
-  } catch (error) {
-    next(error);
-  }
-};
+// const create = async (req, res, next) => {
+//   try {
+//     const newProduct = await productService.create(req.body);
+//     return res.status(201).json(newProduct);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 const getAll = async (req, res, next) => {
   try {
@@ -18,35 +18,37 @@ const getAll = async (req, res, next) => {
   }
 };
 
-const getById = async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const allProducts = await productService.getById(id);
-    return res.status(200).json(allProducts);
-  } catch (error) {
-    next(error);
-  }
-};
+// const getById = async (req, res, next) => {
+//   const { id } = req.params;
+//   try {
+//     const allProducts = await productService.getById(id);
+//     return res.status(200).json(allProducts);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-const update = async (req, res, next) => {
-  const { name, price, urlImage } = req.body;
-  const { id } = req.params;
-  try {
-    await productService.update(id, { name, price, urlImage });
-    return res.status(200).json({ message: 'product updated' });
-  } catch (error) {
-    next(error);
-  }
-};
+// const update = async (req, res, next) => {
+//   const { name, price, urlImage } = req.body;
+//   const { id } = req.params;
+//   try {
+//     await productService.update(id, { name, price, urlImage });
+//     return res.status(200).json({ message: 'product updated' });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-const destroy = async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    await productService.destroy(id);
-    return res.status(204).end();
-  } catch (error) {
-    next(error);
-  }
-};
+// const destroy = async (req, res, next) => {
+//   const { id } = req.params;
+//   try {
+//     await productService.destroy(id);
+//     return res.status(204).end();
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-module.exports = { create, getAll, getById, update, destroy };
+module.exports = { getAll, 
+  // create, getById, update, destroy 
+};
--- back-end/src/utils/validations/validateSaleProduct.js
+++ back-end/src/utils/validations/validateSaleProduct.js
@@ -1,11 +1,11 @@
-const { SaleProduct } = require('../../database/models');
-const { CustomError } = require('../CustomError');
+// const { SaleProduct } = require('../../database/models');
+// const { CustomError } = require('../CustomError');
 
-const validateSaleProduct = async (saleId, productId) => {
-  const saleProduct = await SaleProduct.findOne({ where: { saleId, productId } });
-  if (!saleProduct) {
-    throw new CustomError('Sale or Product not exists.', 404);
-  }
-};
+// const validateSaleProduct = async (saleId, productId) => {
+//   const saleProduct = await SaleProduct.findOne({ where: { saleId, productId } });
+//   if (!saleProduct) {
+//     throw new CustomError('Sale or Product not exists.', 404);
+//   }
+// };
 
-module.exports = { validateSaleProduct };
+// module.exports = { validateSaleProduct };
