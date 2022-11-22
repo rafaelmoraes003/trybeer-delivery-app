689d4a6dc4328908a1a7f7b46e80b0af7456a3cb
rafael moraes <rafaelsm003@gmail.com>
2022-11-22 14:45:27 -0300

    fix: modifica arquivo config do database para aceitar env vars do docker

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,22 +1,11 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
-
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
-
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: 'TRYBEER_DELIVERY_APP',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
